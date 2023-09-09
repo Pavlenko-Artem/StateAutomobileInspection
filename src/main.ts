9458311a86a48a492146bf46ab36f0a3cc818e96
Artem Pavlenko <artempavlenko1703@gmail.com>
2023-09-09 13:24:47 +0300

    Настроен prettier и eslint

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import { AppModule } from './app.module';
 import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.useStaticAssets(join(__dirname, '..', 'public'));
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
+	app.useStaticAssets(join(__dirname, '..', 'public'));
+	app.setBaseViewsDir(join(__dirname, '..', 'views'));
+	app.setViewEngine('hbs');
 
-  await app.listen(3000);
+	await app.listen(3000);
 }
 
 bootstrap();
