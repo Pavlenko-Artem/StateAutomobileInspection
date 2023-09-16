13a1df4b3be5d8bee25f68de16685980703e174d
Artem Pavlenko <artempavlenko1703@gmail.com>
2023-09-16 08:58:03 +0300

    Добавлена работа с шаблонами

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { join } from 'path';
+import * as hbs from 'hbs';
 
 async function bootstrap() {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
 	app.useStaticAssets(join(__dirname, '..', 'public'));
 	app.setBaseViewsDir(join(__dirname, '..', 'views'));
+	hbs.registerPartials(join(__dirname, '..', 'views/partials'));
 	app.setViewEngine('hbs');
 
 	await app.listen(3000);
