2c8604ba5448a85ec5bc0eb3ba314da9ff85a781
Artem Pavlenko <artempavlenko1703@gmail.com>
2023-09-22 14:13:50 +0300

    добавлен просмотр элементов html, теперь не нужно пезапускать сервер чтобы их изменить

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import * as hbs from 'hbs';
+import * as hbsUtils from 'hbs-utils';
 
 async function bootstrap() {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -10,6 +11,9 @@ async function bootstrap() {
 	app.useStaticAssets(join(__dirname, '..', 'public'));
 	app.setBaseViewsDir(join(__dirname, '..', 'views'));
 	hbs.registerPartials(join(__dirname, '..', 'views/partials'));
+	hbsUtils(hbs).registerWatchedPartials(
+		join(__dirname, '..', 'views/partials')
+	);
 	app.setViewEngine('hbs');
 
 	await app.listen(3000);
